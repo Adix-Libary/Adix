f0c499801fce574703d1d94dc8d8d7cb084bb43d
Clean Code <amin.asincorpate@gmail.com>
2024-09-01 13:07:37 -0700

    Add files via upload

---
+++ adix.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+    using namespace std;
+
+string version = "v0.2.0aphla";
+
+int main(int argc, char* argv[]){
+    if(argv[1] = "--v"){
+        cout << "version of adix is " << version << endl;
+    }
+    return 0;
+}
